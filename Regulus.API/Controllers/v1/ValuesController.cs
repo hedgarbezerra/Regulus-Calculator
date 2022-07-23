bba25abeb3156d256c9f3306c103f1cb3b12cc32
Hedgar Bezerra <hedgarbezerra35@outlook.com>
2022-07-22 23:41:19 -0300

    teste

---
+++ Regulus.API/Controllers/v1/ValuesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Regulus.API.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    [Route("api/[controller]")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _env;
+        public ValuesController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_env.EnvironmentName);
+        }
+    }
+}
